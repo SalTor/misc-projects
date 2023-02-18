5e83c2eb93ccad6485c33c7d0358a734616810cf
Salvatore Torcivia <salvatoretorcivia@salvatores-mbp.lan>
2023-02-18 01:14:56 -0500

    set up tailwind configuration

---
+++ packages/config/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["../../packages/ui/**/*.{ts,tsx}", "./pages/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

b173509a1d32effaa51a6ed9ceeefd5040de895f
Salvatore Torcivia <salvatoretorcivia@salvatores-mbp.lan>
2023-02-22 19:45:48 -0500

    add import sort with eslint

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,21 @@
 module.exports = {
   extends: ["next", "turbo", "prettier"],
+  plugins: ["simple-import-sort"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
+    "simple-import-sort/imports": [
+      "error",
+      {
+        groups: [
+          ["^next", "^react"],
+          ["^@?\\w"],
+          ["^src"],
+          ["^src/styles"],
+          ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+          ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+        ],
+      },
+    ],
   },
 };
